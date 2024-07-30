f76ed4c69e547f88ac4d769de93ed13ff46af5ea
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-07-30 16:24:19 +0600

    favicon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -29,6 +29,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import profile from "./profile.png"
 
 export {
   logo,
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  profile
 };
