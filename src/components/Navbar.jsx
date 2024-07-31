ead61adf52e8f19d91c52b160bd9e72c8d10149c
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-07-31 16:55:21 +0600

    hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close, profile } from "../assets";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
