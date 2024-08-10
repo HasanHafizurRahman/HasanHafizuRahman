888244ce92bda8531eeecef2ce5d07e612cb0e95
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-08-10 15:18:09 +0600

    react logo 3d hero

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,27 +5,45 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./react_logo/scene.gltf");
 
   return (
+    // <mesh>
+    //   <hemisphereLight intensity={0.15} groundColor="black" />
+    //   <pointLight intensity={1} />
+    //   <spotLight
+    //     position={[-20, 50, 10]}
+    //     angle={0.12}
+    //     penumbra={1}
+    //     intensity={10}
+    //     castShadow
+    //     shadow-mapSize={1024}
+    //   />
+    //   <primitive
+    //     object={computer.scene}
+    //     scale={0.75}
+    //     position={[0, -3.25, -1.5]}
+    //     rotation={[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={10}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <primitive
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    <hemisphereLight intensity={0.15} groundColor="black" />
+    <pointLight intensity={8} />
+    <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={10}
+      castShadow
+      shadow-mapSize={1024}
+    />
+    <primitive
+      object={computer.scene}
+      scale={1.10}
+      position={[0, -2.65, -1]}
+      rotation={[0, -2, 0]}
+    />
+  </mesh>
   );
 };
 
